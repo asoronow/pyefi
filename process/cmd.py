f19e4175a597f7edeb9375eaf760d98751470977
Alec <a.soronow@gmail.com>
2021-03-15 14:56:27 -0700

    removed prewitt, added sobel, command line args, program now ready for use/testing

--- process/cmd.py
+++ process/cmd.py
@@ -1,21 +1,44 @@
-import argparse
+import argparse, os
 from focus import TIFF
 class CommandLine():
     '''
     Accept arguments and prints statuses
     '''
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
+        self.parser = argparse.ArgumentParser(description = 'pyefi: OME-TIFF Focus Stacking', 
+                                             epilog = 'Takes a single file or directory of OME-TIFF files and focus stacks each channel.',  
+                                             add_help = True, 
+                                             prefix_chars = '-', 
+                                             usage = '%(prog)s [options] <input file/dir> <output dir>' 
+                                             )
+        self.parser.add_argument('-a','--algorithm', action = 'store', choices=('laplace','sobel', 'canny') , default = 'sobel', help='The algorithm to use for focus detection. Defautl sobel.' )
+        self.parser.add_argument('-lk','--lapkernel',action = 'store', type = int, default=13, help = 'Size of the kernel used for LoG, must be odd and <= 31')
+        self.parser.add_argument('-sk','--sobelkernel',action = 'store', type = int, default=5, help = 'Size of the kernel used for Sobel, must be 1,3,5, or 7')
+        self.parser.add_argument('-cl','--cannylo',action = 'store', type = int, default=100, help = 'Lower Canny threshold')
+        self.parser.add_argument('-ch','--cannyhi',action = 'store', type = int, default=255, help = 'Upper Canny threshold')
+        self.parser.add_argument('-p','--prefix',action = 'store', type = str, default= 'stacked_', help = 'Name to append to the start of output file names')
+        self.parser.add_argument('input')
+        self.parser.add_argument('output')
 
-        # The required positional arg
-        self.parser.add_argument("input", help="The file or directory you wish to process (OME-TIFF format only!)")
-
-        optionals = {}
+        self.args = self.parser.parse_args()
 
 def main():
     ''' Gets command line parameters executes program. '''
-    defaults = {
-        "lapKernel":13, 
-        "lapBlur":13
-        }
-    TIFF("m107.ome.tif", defaults).getFocused()
+    params = CommandLine().args
+
+    if os.path.exists(params.output) and os.path.exists(params.input): # Make sure we can reach these
+        if os.path.isdir(params.input): # We need to grab all the files we can process
+            print("Loading files and processing, this could take a while...")
+            omeTIFF = [] # List of TIFF objects to process
+            for (dirpath, dirnames, filenames) in os.walk(params.input):
+                for fname in filenames:
+                    if 'ome.tif' in fname or 'ome.tiff' in fname: # Check the two most common extensions
+                        omeTIFF.append(TIFF(os.path.join(dirpath, fname), params))
+            for tiff in omeTIFF:
+                tiff.getFocused()
+        elif os.path.isfile(params.input): # If this is just a file then process it
+            print("Loading file and processing, this could take a while...")
+            TIFF(params.input, params).getFocused()
+
+    else: # Tell them something is wrong
+        print("Your input or output path was invalid!")
--- process/focus.py
+++ process/focus.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, os
 import tifffile as tf
 import numpy as np
 
@@ -14,35 +14,40 @@ class TIFF():
     def __init__(self, fname, params):
         ''' Store key parameters. Split the channels into their respectve zstacks. '''
         self.fname = fname
-        self.raw = tf.imread(self.fname)
-        self.channels = [[] for i in range(self.raw.shape[1])]
-        self.params = params
-        for c in range(0,self.raw.shape[1]):
+        self.outfile = fname.replace(params.input+"/", params.prefix) # Apply the prefix for output
+        self.raw = tf.imread(self.fname) # Raw tiff data
+        self.channels = [[] for i in range(self.raw.shape[1])] # list of channels
+        self.params = params # Command line arguments
+        for c in range(0,self.raw.shape[1]): # Read channels into list
             for zstack in self.raw[:,c,:,:]:
-                self.channels[c].append((zstack/256).astype('uint8'))
-                cv2.imwrite('check.png', (zstack/256).astype('uint8'))
+                if zstack.dtype == 'uint16': # We can only handle 8-bit values with our detector
+                    self.channels[c].append((zstack/256).astype('uint8')) # Convert to 8-bit if necessary
+                elif zstack.dtype == 'uint8':
+                    self.channels[c].append(zstack)
+
         
     def __homography(self, zstackKeyPoints, motherKeyPoints, matched):
         ''' Finds homography between two images to warp images for alignment. '''
+        # Empty arrays to fill in with matched points
         zstackPoints = motherPoints = np.zeros((len(matched), 1, 2), dtype=np.float32)
 
-        for i in range(0,len(matched)):
+        for i in range(0,len(matched)): # We train the detector with the kp of the mother image against the zstack
             zstackPoints[i] = zstackKeyPoints[matched[i].queryIdx].pt
             motherPoints[i] = motherKeyPoints[matched[i].trainIdx].pt
 
-
+        # Using RANSAC to approximate the locations of absolute homography between images
         homography, mask = cv2.findHomography(zstackPoints, motherPoints, cv2.RANSAC, ransacReprojThreshold=2.0)
 
         return homography
         
     def __align(self):
-        ''' Aligns the images of each channel in the TIFF. '''
-        # ORB is fast and works well for most images
-        detector = cv2.ORB_create()
+        ''' Aligns the images of each channel in the TIFF using ORB (Oriented FAST and Rotated BRIEF). '''
+        # ORB is fast and works well for most images (also not patented)
+        detector = cv2.ORB_create(1500) # 1500 is the number of features to retain, higher accuracy
 
-        # Prepare the results table
+        # Prepare the results list
         results = [[] for channel in self.channels]
-        c = 0
+        c = 0 # Channel counter
         for channel in self.channels:
             mother = channel[0] # Take the top image as the mother iamge to align to
             # Get the key points and descriptor using the detector
@@ -51,7 +56,7 @@ class TIFF():
                 zstackKeyPoints, zstackDesc = detector.detectAndCompute(channel[zstack], None)
 
                 # If we are using ORB then we should just brute force it
-                # Hamming and crosscheck to attempt some error correction
+                # Hamming distance and crosscheck for feature matching
                 matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
                 raw = matcher.match(zstackDesc, motherDesc)
                 
@@ -67,10 +72,10 @@ class TIFF():
 
         return results
 
-    def __findLaplacian(self, image):
-        ''' Computes Laplacian of the image getting what's in focus (edges really). '''
+    def __findLoG(self, image):
+        ''' Computes Laplacian of the Gaussian of the image getting what's in focus. '''
         # Grab the specified parameters, this defaults to 5,5
-        kernel, blur = self.params.get('lapKernel'), self.params.get('lapBlur')
+        kernel = blur = self.params.lapkernel
         # Apply blur so we can easily calculate the laplacian
         gauss = cv2.GaussianBlur(image, (blur,blur), 0) 
         # Calculate the laplacian
@@ -78,33 +83,55 @@ class TIFF():
 
         return laplace
 
-    def __findPrewitt(self, image):
-        ''' Compute Prewitt's operator on the image to find what's in focus. '''
+    def __findCanny(self, image):
+        ''' Computes Canny edge detection on the image to find in focus. '''
+        return cv2.Canny(image, self.params.cannylo, self.params.cannyhi)
+
+    def __findSobel(self, image):
+        ''' Compute Sobel gradient of the image to find what's in focus. '''
+        kernel = blur = self.params.sobelkernel # Kernel and blur for sobel
+
+        gauss = cv2.GaussianBlur(image, (blur,blur), 0) # Apply blur
+
+        sobelx = cv2.Sobel(gauss, cv2.CV_8U, 1, 0, ksize=kernel) # Sobel x calculation
+        sobely = cv2.Sobel(gauss, cv2.CV_8U, 0, 1, ksize=kernel) # Sobel y calculation
+
+        return sobelx + sobely # Sum the two to get the complete gradient
+
     def __doRebuild(self, channels=[]):
         ''' Rebuilds the TIFF using the focus stacked channels. '''
-        print(np.asarray(channels).shape)
-        tf.imwrite('resutl.tif', np.asarray(channels), photometric='minisblack')
+        tf.imwrite(
+            os.path.join(self.params.output, self.outfile), # Output file path
+            np.asarray(channels), # The newly stacked channels
+            photometric='minisblack' # Tell the module these are grayscale, RGB gets funky
+        )
 
-    def getFocused(self, tiffPerChannel=False):
+        print(f"Finished processing {self.fname}")
+
+    def getFocused(self):
         ''' Focus stacks each individual channel and then rebuilds TIFF for output. '''
-        aligned = self.__align()
+        aligned = self.__align() # The aligned channels
+        # Helpful dictionary to select the algo
+        algos = {'canny':self.__findCanny,'laplace':self.__findLoG,'sobel':self.__findSobel}
+        # List of the final focused channels
         focused = []
+
+        print(f"Starting processing for {self.fname}")
+
         for channel in aligned:
             processed = []
             for zstack in channel:
-                processed.append(self.__findLaplacian(zstack))
+                processed.append(algos[self.params.algorithm](zstack)) # Use the algo of choice to find in focus
 
-            stacked = np.zeros(shape=channel[0].shape, dtype=channel[0].dtype)
+            output = np.zeros(shape=channel[0].shape, dtype=channel[0].dtype) # Blank array in the shape of final image
             
-            absolute = np.absolute(np.asarray(processed))
-            maxima = absolute.max(axis=0)
-            boolMask = processed == maxima
-            mask = boolMask.astype(np.uint8)
+            absolute = np.absolute(np.asarray(processed)) # Get the absolute values of the mask
+            boolMask = absolute == absolute.max(axis=0) # We only want the maxima, making it boolean to apply bitwise
+            mask = boolMask.astype(np.uint8) # convert here so we are sure of captability
 
             for zstack in range(0,len(channel)):
-                stacked = cv2.bitwise_not(channel[zstack], stacked, mask=mask[zstack])
+                output = cv2.bitwise_not(channel[zstack], output, mask=mask[zstack]) # Apply our newly found mask
 
-            focused.append(255-stacked)
-        
-        if not tiffPerChannel:
-            self.__doRebuild(focused)
+            focused.append(255-output) # These values are flipped we want black as the minimum value
+
+        self.__doRebuild(focused) # Recreate the new TIFF file
